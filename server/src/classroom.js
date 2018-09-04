71236e160630fe5eaee80f2a09aa9ccb8824ede1
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-09-04 22:56:03 +0200

    Removed symbols to create private properties on classroom

--- server/src/classroom.js
+++ server/src/classroom.js
@@ -10,70 +10,76 @@ http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method
 
  */
 
-const _students = Symbol('_students');
-const _statusSubject = Symbol('_statusSubject');
-const _statusStream =Symbol('_statusStream');
+// I dont like this, is ugly
 
-const _initializeStatusStream = Symbol('_initializeStatusStream');
-const _initializeStatusSubject = Symbol('_initializeStatusSubject');
-const _getStatusSubject = Symbol('_getStatusSubject');
-const _notify = Symbol('_notify');
+// const _students = Symbol('_students');
+// const _statusSubject = Symbol('_statusSubject');
+// const _statusStream =Symbol('_statusStream');
+//
+// const _initializeStatusStream = Symbol('_initializeStatusStream');
+// const _initializeStatusSubject = Symbol('_initializeStatusSubject');
+// const _getStatusSubject = Symbol('_getStatusSubject');
+// const _notify = Symbol('_notify');
 
 class Classroom {
 
     constructor(){
-        this[_students] = new Students();
-    }
-
-    [_notify]() {
-        let state = this.getState();
-        let subject = this[_getStatusSubject]();
-        subject.next(state);
+        this._students = new Students();
     }
 
     connect(studentId) {
-        this[_students].add(studentId);
-        this[_notify]();
+        this._students.add(studentId);
+        this._notify();
     }
 
     disconnect(studentId) {
-        this[_students].remove(studentId);
+        this._students.remove(studentId);
     }
 
     count() {
-        return this[_students].count();
+        return this._students.count();
     }
 
     getState(){
-        return this[_students].getState();
+        return this._students.getState();
     }
 
     isFollowing(studentId) {
-        this[_students].get(studentId).state = STUDENT_STATE.FOLLOWING;
-        this[_notify]();
+        this._students.get(studentId).state = STUDENT_STATE.FOLLOWING;
+        this._notify();
     }
 
     hasLost(studentId) {
-        this[_students].get(studentId).state = STUDENT_STATE.LOST;
-        this[_notify]();
+        this._students.get(studentId).state = STUDENT_STATE.LOST;
+        this._notify();
     }
 
     getStatusStream() {
-        return this[_statusStream] || this[_initializeStatusStream]();
+        return this._statusStream || this._initializeStatusStream();
     }
 
-    [_initializeStatusSubject]() {
-        this[_statusSubject] = new BehaviorSubject(this.getState());
-        return this[_statusSubject];
+    //---------------------------------------------------
+    // "Private" methods
+    //---------------------------------------------------
+
+    _initializeStatusSubject() {
+        this._statusSubject = new BehaviorSubject(this.getState());
+        return this._statusSubject;
     }
 
-    [_getStatusSubject]() {
-        return this[_statusSubject] || this[_initializeStatusSubject]();
+    _getStatusSubject() {
+        return this._statusSubject || this._initializeStatusSubject();
     }
 
-    [_initializeStatusStream]() {
-        this[_statusStream] = this[_getStatusSubject]().asObservable();
-        return this[_statusStream];
+    _initializeStatusStream() {
+        this._statusStream = this._getStatusSubject().asObservable();
+        return this._statusStream;
+    }
+
+    _notify() {
+        let state = this.getState();
+        let subject = this._getStatusSubject();
+        subject.next(state);
     }
 
 }
